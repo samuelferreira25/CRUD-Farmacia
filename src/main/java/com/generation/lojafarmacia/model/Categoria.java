0b269774015ae703f8557ec2de467a1a2dd3c395
samuelferreira25 <samuel.ferreira2014.sf@gmail.com>
2024-01-31 11:16:17 -0300

    Atualização 3

--- src/main/java/com/generation/lojafarmacia/model/Categoria.java
+++ src/main/java/com/generation/lojafarmacia/model/Categoria.java
@@ -1,19 +1,28 @@
 package com.generation.lojafarmacia.model;
 
 
-import jakarta.persistence.Entity;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "tb_categoria")
 public class Categoria {
-	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria" , cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto>produto;
+
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 private Long id;
