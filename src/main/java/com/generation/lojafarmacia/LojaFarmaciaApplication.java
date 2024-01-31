03e319c6202e3229ed930c1798689bda20084334
samuelferreira25 <samuel.ferreira2014.sf@gmail.com>
2024-01-31 10:16:11 -0300

    Autualização

---
+++ src/main/java/com/generation/lojafarmacia/LojaFarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.lojafarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaFarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaFarmaciaApplication.class, args);
+	}
+
+}
